833278e0aa87ac5c9db3f61ea4204e6055aaf2d3
Olegario <olegario.fernandezalonso@vtsd.com>
2022-10-28 07:41:26 -0400

    Code Update

--- Lab05vst.java
+++ Lab05vst.java
@@ -6,17 +6,67 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
         int width = 980;
         int height = 630;
         int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
-        g.drawRect(10,10,width,height);
+        g.drawRect(10, 10, width, height);
+
+
+        for (int k = 0; k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y2 -= 18;
+            x1 += 28;
+        }
+        for (int k = 0; k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 -= 18;
+            x2 -= 28;
+        }
+        for (int k = 0; k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y2 += 18;
+            x1 -=28;
+        }
+        for (int k = 0; k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 += 18;
+            x2 += 28;
+        }
+        //Extra Points
+        int width2 = 500;
+        int height2 = 300;
+        int x3 = 250;
+        int y3 = 475;
+        int x4 = 750;
+        int y4 = 475;
+        g.drawRect(250, 175, width2, height2);
+
+        for (int k = 0; k < 20; k++) {
+            g.drawLine(x3, y3, x4, y4);
+            y4 -= 15;
+            x3 += 25;
+        }
+        for (int k = 0; k < 20; k++) {
+            g.drawLine(x3, y3, x4, y4);
+            y3 -= 15;
+            x4 -= 25;
+        }
+        for (int k = 0; k < 20; k++) {
+            g.drawLine(x3, y3, x4, y4);
+            y4 += 15;
+            x3 -=25;
+        }
+        for (int k = 0; k < 20; k++) {
+            g.drawLine(x3, y3, x4, y4);
+            y3 += 15;
+            x4 += 25;
+        }
+
     }
 }
 
--- Temp.java
+++ Temp.java
@@ -1,2 +1,22 @@
-public class Temp {
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Temp extends Applet {
+
+    public void paint(Graphics g) {
+        int x = 20;
+        int y = 20;
+        int width = 50;
+        int height = 50;
+
+        for (int k = 0; k < 50; k++) {
+            g.drawOval(x, y, width, height);
+            x +=20;
+            y +=20;
+        }
+    }
+
+
 }
